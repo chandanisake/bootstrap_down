0157d29e85abeee5445d5db25599af4141cd974f
chandanisake <chandanisake2002gmail.com>
2023-08-10 13:31:30 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render
+
+# Create your views here.
+def bootstrap_download(request):
+    return render(request,'bootstrap_download.html')
+
+
+
+def buttongroup(request):
+    return render(request,'buttongroup.html')
+
+
+def alerts(request):
+    return render(request,'alerts.html')
+
+
+def accordion(request):
+    return render(request,'accordion.html')
+
+def badge(request):
+    return render(request,'badge.html')
+
+def breadcrumb(request):
+    return render(request,'breadcrumb.html')
+
+def buttons(request):
+    return render(request,'buttons.html')
+
+def bg(request):
+    return render(request,'bg.html')
+
+def card(request):
+    return render(request,'card.html')
+
+
+def carousel(request):
+    return render(request,'carousel.html')
+
+def forms(request):
+    return render(request,'forms.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
